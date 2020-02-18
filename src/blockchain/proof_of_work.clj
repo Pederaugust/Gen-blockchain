1cb9313a009ba6ad922ffc5a8dbb96cc59222b4f
peder.august <peder.august@protonmail.com>
2020-02-18 06:52:27 +0100

    Removed proof of work

--- src/blockchain/proof_of_work.clj
+++
@@ -1,5 +0,0 @@
-(ns blockchain.proof-of-work
-  (:require [buddy.core.hash :as hash]
-            [buddy.core.codecs :as codecs]
-            [clj-time.core :as time]
-            [clj-time.coerce :as coerce]))
